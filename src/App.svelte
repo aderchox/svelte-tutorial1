4afb15b50c0aaaea767e92262f6f0488e03b76a1
aderchox <aderchox@gmail.com>
2024-03-29 14:48:07 +0330

    tab component done#1

--- src/App.svelte
+++ src/App.svelte
@@ -4,26 +4,33 @@
   const tabsData = [
     {
       title: "AAA",
-      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      content: "This is bare content. Lorem ipsum dolor sit amet consectetur adipisicing elit."
     },
     {
       title: "BBB",
-      heading: "Dolor amet consectetur",
+      heading: "This is heading. Dolor amet consectetur",
       content: `
       <h3>This is structured content.</h3>
       <p>This is a whole test paragraph, still in the content.</p>
       `
     },
   ]
+  let activeTitle = tabsData[0].title;
 </script>
 
 <main>
-  <!-- <input type="text" on:input={(e) => console.log(e.data)}> -->
-
-  <Tabs data={tabsData}>
-    <h2 slot="title" let:title class="title">
-      {title}
-    </h2>
+  <Tabs data={tabsData} {activeTitle}>
+    <ul slot="titles" let:titles class="titles" role="list">
+      {#each titles as title}
+        <li
+          class="title"
+          class:active={activeTitle === title}
+          on:click={e => activeTitle = title}
+        >
+          <h2>{title}</h2>
+        </li>
+      {/each}
+    </ul>
     <h3 slot="heading" let:heading class={heading ? "heading" : ""}>
       {heading ?? ""}
     </h3>
@@ -35,7 +42,13 @@
 </main>
 
 <style>
+  /* Styling is possible from here too. */
   .tabPage {
     border: 1px solid #eee;
+    margin-top: 10px;
   }
+  /* Sounds like this is not possible to target things inside an @html from here. */
+  /* .tabPage > h3 {
+    margin: 0;
+  } */
 </style>
--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -1,21 +1,39 @@
 <script>
     export let data;
+    export let activeTitle;
 </script>
 
 <div>
+    <slot name="titles" titles={data.map(item => item.title)} />
     {#each data as item}
-        <slot name="title" title={item.title} />
-        <slot name="heading" heading={item.heading} />
-        <slot name="content" content={item.content} />
+        {#if item.title === activeTitle}
+            <slot name="heading" heading={item.heading} />
+            <slot name="content" content={item.content} />
+        {/if}
     {/each}
 </div>
 
 <style>
+    div :global(.titles) {
+        padding: 0;
+        display: flex;
+        gap: 20px;
+    }
     div :global(.title) {
         user-select: none;
         cursor: pointer;
     }
+    div :global(.title.active) {
+        color: red;
+    }
+    div :global(.title:hover) {
+        color: rgb(245, 125, 125);
+    }
     div :global(.heading) {
+        padding: 20px;
         border: 1px solid red;
+    }
+    div :global(.tabPage) {
+        padding: 20px;
     }
 </style>

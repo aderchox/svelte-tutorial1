e08530c77bd77e6b9b27cc7a0d57759684820b43
aderchox <aderchox@gmail.com>
2024-03-29 13:42:00 +0330

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Tabs from "./lib/Tabs.svelte";
+
+  const tabsData = [
+    {
+      title: "AAA",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+    },
+    {
+      title: "BBB",
+      heading: "Dolor amet consectetur",
+      content: `
+      <h3>This is structured content.</h3>
+      <p>This is a whole test paragraph, still in the content.</p>
+      `
+    },
+  ]
+</script>
+
+<main>
+  <!-- <input type="text" on:input={(e) => console.log(e.data)}> -->
+
+  <Tabs data={tabsData}>
+    <h2 slot="title" let:title class="title">
+      {title}
+    </h2>
+    <h3 slot="heading" let:heading class={heading ? "heading" : ""}>
+      {heading ?? ""}
+    </h3>
+    <div slot="content" let:content class="tabPage">
+      <!-- XSS warning: Be careful to feed it yourself! -->
+      {@html content}
+    </div>
+  </Tabs>
+</main>
+
+<style>
+  .tabPage {
+    border: 1px solid #eee;
+  }
+</style>
---
+++ src/lib/Tabs.svelte
@@ -0,0 +1,21 @@
+<script>
+    export let data;
+</script>
+
+<div>
+    {#each data as item}
+        <slot name="title" title={item.title} />
+        <slot name="heading" heading={item.heading} />
+        <slot name="content" content={item.content} />
+    {/each}
+</div>
+
+<style>
+    div :global(.title) {
+        user-select: none;
+        cursor: pointer;
+    }
+    div :global(.heading) {
+        border: 1px solid red;
+    }
+</style>
